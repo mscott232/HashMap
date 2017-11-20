aa106f2d119c7f98ec1982635c6c78683ec07dec
Matt Scott <mscott232@gmail.com>
2017-11-19 22:26:57 -0600

    Created StringKey class. Created method stubs for all methods included in class.

---
+++ HashMap/StringKey.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HashMap
+{
+    class StringKey : IComparable<StringKey>
+    {
+        private string keyName;
+        private static double COEFFICIENT = 31;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="keyName"></param>
+        public StringKey(string keyName)
+        {
+            this.keyName = keyName;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="o"></param>
+        /// <returns></returns>
+        public override bool Equals(object o)
+        {
+            return true;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public string GetKeyName()
+        {
+            return keyName;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public override string ToString()
+        {
+            return "";
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="other"></param>
+        /// <returns></returns>
+        public int CompareTo(StringKey other)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
